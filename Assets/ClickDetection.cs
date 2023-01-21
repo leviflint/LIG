748a8423953e074934d42d6c4faab84e1a76b910
Phaytx <xphayt@gmail.com>
2023-01-21 13:48:53 -0500

    first commit

---
+++ Assets/ClickDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickDetection : MonoBehaviour
+{
+
+    public Button mouseclick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouseclick.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You clicked that shit homie!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
