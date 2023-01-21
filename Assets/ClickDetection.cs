07c134ee10e6c0e8af1d6b88b550f95ec0f79368
Phaytx <xphayt@gmail.com>
2023-01-21 16:08:40 -0500

    Fix text counter not updating!

--- Assets/ClickDetection.cs
+++ Assets/ClickDetection.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static Wood;
 
 public class ClickDetection : MonoBehaviour
 {
 
     public Button mouseclick;
+    Wood wood = new Wood();
 
     // Start is called before the first frame update
     void Start()
     {
         mouseclick.onClick.AddListener(TaskOnClick);
+        Debug.Log("Listener Started");
     }
 
     void TaskOnClick()
     {
-        Debug.Log("You clicked that shit homie!");
+        wood.SetWood();
     }
 
     // Update is called once per frame
---
+++ Assets/WoodCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static Wood;
+
+public class WoodCounter : MonoBehaviour
+{
+    Text woodText;
+    Wood wood = new Wood();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        woodText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        woodText.text = "Wood: " + wood.GetWood();
+        Debug.Log(wood.GetWood());
+    }
+}
