60e2abeb9c158a934e4df8225b5ce5b489833b01
leviflint <tweekex@hotmail.com>
2023-01-22 20:36:05 -0500

    Created player data.

---
+++ Assets/Data/PlayerData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    public string PlayerName;
+    public Sprite PlayerImage;
+    public int TotalWood;
+    public float WoodMultiplyer;
+    public float ClickMultiplyer;
+
+}
